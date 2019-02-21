05a25b2b025e3273488617c7bec56386793ea3bd
salty68 <lck295@nyu.edu>
2019-02-21 11:26:49 -0500

    First Commit

---
+++ Milestone/Assets/Player_Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour {
+
+    public Rigidbody myRigidBody;
+
+    public Vector2 vectorInput;
+
+    public float forceMultiplier;
+    public float torqueMultiplier;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputVertical = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        vectorInput = new Vector2(inputHorizontal, inputVertical);
+	}
+}
